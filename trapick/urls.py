72780fc2de53583c7b7611c88d238a27bc7403c0
NurAli <gt201901065@wmsu.edu.ph>
2026-02-13 11:05:49 +0800

    ereda

--- trapick/urls.py
+++ trapick/urls.py
@@ -14,26 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#trapick/urls.py
+# trapick/urls.py
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-import os
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name='index.html'), name='home'),
     path("admin/", admin.site.urls),
-    path('api/', include("trapickapp.urls")),  # Prefix API routes
+    path('api/', include("trapickapp.urls")),
 ]
 
-# Serve React frontend at root path
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    # Production: serve React app
-    urlpatterns += [
-        path('', TemplateView.as_view(template_name='index.html'), name='home'),
-        # Catch-all for React Router
-        path('<path:route>', TemplateView.as_view(template_name='index.html')),
-    ]
